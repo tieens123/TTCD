f1e1e1efed0d906dac80bb8099450aa8df06ccfe
tieens123 <tienhss2k3@gmail.com>
2023-10-22 21:14:18 +0700

    day30

---
+++ ASP.NET/lab04/lab04/Controllers/PeopleController.cs
@@ -0,0 +1,134 @@
+﻿using lab04.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.IO;
+
+namespace lab04.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: PeopleController
+        public ActionResult Index()
+        {
+            var _peoples = DataLoal.GetPeoples();
+            return View(_peoples);
+        }
+
+        // GET: PeopleController/Details/5
+        public ActionResult Details(int id)
+        {
+            var peoples = DataLoal.GetPeopleById(id);
+            return View(peoples);
+        }
+
+        // GET: PeopleController/Create
+        public ActionResult Create()
+        {
+            People people = new People();
+            return View(people);
+        }
+
+        // POST: PeopleController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(People model)
+        {
+            try
+            {
+                //upload file và thư mục wwwroot/images/avatar
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count() >0 && files[0].Length > 0)
+                {
+                    var file = files[0];
+                    var FileName = file.FileName;
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\avatar", FileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                        model.Avatar = "images/avatar/" + FileName;
+                    }
+                }
+                DataLoal._peoples.Add(model);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                ViewBag.error = ex.Message;
+                return View(model);
+            }
+        }
+
+        // GET: PeopleController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var people = DataLoal.GetPeopleById(id);
+            return View(people);
+        }
+
+        // POST: PeopleController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, People model)
+        {
+            try
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count() >0 && files[0].Length >0)
+                {
+                    var file = files[0];
+                    var FileName = file.FileName;
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\avatar", FileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                        model.Avatar = "images/avatar/" + FileName;
+                    }
+                }
+                for (int i = 0; i<DataLoal._peoples.Count;i++)
+                {
+                    if (DataLoal._peoples[i].Id == id)
+                    {
+                        DataLoal._peoples[i] = model;
+                        break;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+
+        // GET: PeopleController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var peoples = DataLoal.GetPeopleById(id);
+            return View(peoples);
+        }
+
+        // POST: PeopleController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, People model)
+        {
+            try
+            {
+                for (int i = 0; i < DataLoal._peoples.Count; i++)
+                {
+                    if (DataLoal._peoples[i].Id == id)
+                    {
+                        DataLoal._peoples.RemoveAt(i);
+                        break;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ASP.NET/lab04/lab04/Models/DataLoal.cs
@@ -0,0 +1,55 @@
+﻿namespace lab04.Models
+{
+    public class DataLoal
+    {
+        public static List<People> _peoples = new List<People>()
+        {
+            new People()
+            {
+                Id = 0,
+                Name = "Devmaster",
+                Email = "devmaster.edu.vn",
+                Phone = "0987654321",
+                Address = "25 VŨ NGỌC PHAN",
+                Avatar = "images/avatar/01.jpg",
+                Birthday = Convert.ToDateTime("21/05/2123"),
+                Bio = "devmaster",
+                Gender = 0
+            },
+            new People()
+            {
+                Id = 1,
+                Name = "Tien",
+                Email = "tienhss2k3@gmail.com",
+                Phone = "0383428834",
+                Address = "unknow",
+                Avatar = "images/avatar/02.jpg",
+                Birthday = Convert.ToDateTime("21/05/2003"),
+                Bio = "devmaster",
+                Gender = 1
+            },
+            new People()
+            {
+                Id = 2,
+                Name = "Linh",
+                Email = "pttl2k32gmail.com",
+                Phone = "0987654321",
+                Address = "unknow",
+                Avatar = "images/avatar/03.jpg",
+                Birthday = Convert.ToDateTime("14/11/2003"),
+                Bio = "devmaster",
+                Gender = 2
+            },
+        };
+        public static List<People> GetPeoples()
+        {
+            return _peoples;
+        }
+        public static People? GetPeopleById(int Id)
+        {
+            var people = _peoples.FirstOrDefault(x => x.Id == Id);
+            return people;
+        }
+
+    }
+}
